41d81d75da907e02c071e5844cea8f98322b6208
Curt Grogan <curtgrogan@outlook.com>
2019-12-04 15:19:48 -0500

    API is functional - returns data for GruInc

--- src/pages/Minions.jsx
+++ src/pages/Minions.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const Minions = () => {
+  const [employees, setEmployees] = useState([])
+
+  const getEmployees = async () => {
+    const apiUrl =
+      'https://sdg-staff-directory-app.herokuapp.com/api/GruInc/Employees'
+    const resp = await axios.get(apiUrl)
+    setEmployees(resp)
+    console.log(resp)
+  }
+  // Use Effect for Page Render
+  useEffect(() => {
+    // do something here
+    console.log('Rendered - useEffect []')
+    getEmployees()
+  }, [])
   return (
-    <div>
+    <>
       <Link className="employeeName" to={'/minion/'}>
         Link to Minion Page
       </Link>
-    </div>
+    </>
   )
 }
 
