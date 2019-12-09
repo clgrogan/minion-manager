3a43574f151998c285134c98bd7a750c7e5ac99f
Curt Grogan <curtgrogan@outlook.com>
2019-12-09 00:03:03 -0500

    gitter done

--- src/pages/AddMinion.jsx
+++ src/pages/AddMinion.jsx
@@ -43,10 +43,10 @@ const AddMinion = () => {
   const addEmployeeApi = async e => {
     e.preventDefault()
     console.log(employee)
-    return
     const apiUrl =
       'https://sdg-staff-directory-app.herokuapp.com/api/GruInc/Employees'
     const resp = await axios.post(apiUrl, employee)
+    console.log(resp)
   }
 
   return (
@@ -54,8 +54,8 @@ const AddMinion = () => {
       <section className="formSection">
         <form className="addEmployeeForm" onSubmit={addEmployeeApi}>
           <section className="formInputSection">
-            <div className="inputGroup">
-              <label className="firstName">First Name</label>
+            <label className="firstName">
+              First Name:
               <input
                 type="text"
                 name="firstName"
@@ -64,9 +64,221 @@ const AddMinion = () => {
                 onChange={handleInputOnChange}
                 required
               />
-            </div>
+            </label>
+            <label className="lastName">
+              Last Name:
+              <input
+                type="text"
+                name="lastName"
+                id="lastName"
+                value={employee.lastName}
+                onChange={handleInputOnChange}
+                required
+              />
+            </label>
+            <label className="profileImage">
+              Profile Img Url:
+              <input
+                type="text"
+                name="profileImage"
+                id="profileImage"
+                value={employee.profileImage}
+                onChange={handleInputOnChange}
+                required
+              />
+            </label>
+            <label className="birthday">
+              Birth Date:
+              <input
+                type="date"
+                name="birthday"
+                id="birthday"
+                value={employee.birthday}
+                onChange={handleInputOnChange}
+                required
+              />
+            </label>
+            <label className="hiredDate">
+              Hire Date:
+              <input
+                type="date"
+                name="hiredDate"
+                id="hiredDate"
+                value={employee.hiredDate}
+                onChange={handleInputOnChange}
+                required
+              />
+            </label>
+            <label className="jobTitle">
+              Title:
+              <input
+                type="text"
+                name="jobTitle"
+                id="jobTitle"
+                value={employee.jobTitle}
+                onChange={handleInputOnChange}
+                required
+              />
+            </label>
+            <label className="jobDescription">
+              Job Desc.:
+              <input
+                type="text"
+                name="jobDescription"
+                id="jobDescription"
+                value={employee.jobDescription}
+                onChange={handleInputOnChange}
+                required
+              />
+            </label>
+            <label className="isFullTime">
+              Full Time Employee:
+              <p>
+                <input
+                  className="checkBoxInput"
+                  type="checkbox"
+                  defaultChecked="{employee.isFullTime}"
+                  name="isFullTime"
+                  id="isFullTime"
+                  value={employee.isFullTime}
+                  onChange={handleInputOnChange}
+                />
+                (check box for yes)
+              </p>
+            </label>
+            <label className="ptoHours">
+              PTO (Hours):
+              <input
+                type="text"
+                name="ptoHours"
+                id="ptoHours"
+                value={employee.ptoHours}
+                onChange={handleInputOnChange}
+                required
+              />
+            </label>
+            <label className="salary">
+              Salary:
+              <input
+                type="text"
+                name="salary"
+                id="salary"
+                value={employee.salary}
+                onChange={handleInputOnChange}
+                required
+              />
+            </label>
+            <label className="interestingFact">
+              Interesting Fact:
+              <input
+                type="text"
+                name="interestingFact"
+                id="interestingFact"
+                value={employee.interestingFact}
+                onChange={handleInputOnChange}
+                required
+              />
+            </label>
+            <label className="gender">
+              Gender:
+              <input
+                type="text"
+                name="gender"
+                id="gender"
+                value={employee.gender}
+                onChange={handleInputOnChange}
+                required
+              />
+            </label>
+            <label className="address">
+              Address:
+              <input
+                type="text"
+                name="address"
+                id="address"
+                value={employee.address}
+                onChange={handleInputOnChange}
+                required
+              />
+            </label>
+            <label className="city">
+              City:
+              <input
+                type="text"
+                name="city"
+                id="city"
+                value={employee.city}
+                onChange={handleInputOnChange}
+                required
+              />
+            </label>
+            <label className="state">
+              State:
+              <input
+                type="text"
+                name="state"
+                id="state"
+                value={employee.state}
+                onChange={handleInputOnChange}
+                required
+              />
+            </label>
+            <label className="zip">
+              Zip:
+              <input
+                type="text"
+                name="zip"
+                id="zip"
+                value={employee.zip}
+                onChange={handleInputOnChange}
+                required
+              />
+            </label>
+            <label className="phoneNumber">
+              Phone #:
+              <input
+                type="text"
+                name="phoneNumber"
+                id="phoneNumber"
+                value={employee.phoneNumber}
+                onChange={handleInputOnChange}
+                required
+              />
+            </label>
+            <label className="emergencyContactPerson">
+              Emerg. Contact:
+              <input
+                type="text"
+                name="emergencyContactPerson"
+                id="emergencyContactPerson"
+                value={employee.emergencyContactPerson}
+                onChange={handleInputOnChange}
+                required
+              />
+            </label>
+            <label className="emergencyContactPhone">
+              Emerg. Ctct. #:
+              <input
+                type="text"
+                name="emergencyContactPhone"
+                id="emergencyContactPhone"
+                value={employee.emergencyContactPhone}
+                onChange={handleInputOnChange}
+                required
+              />
+            </label>
+            <label className="emergencyContactAddress">
+              Emerg. Ctct. Address:
+              <input
+                type="text"
+                name="emergencyContactAddress"
+                id="emergencyContactAddress"
+                value={employee.emergencyContactAddress}
+                onChange={handleInputOnChange}
+                required
+              />
+            </label>
           </section>
-
           <button className="addEmployeeBtn" type="submit">
             Add Employee
           </button>
@@ -77,29 +289,3 @@ const AddMinion = () => {
 }
 
 export default AddMinion
-
-// {
-//   "id": 0,
-//   "firstName": "string",
-//   "lastName": "string",
-//   "birthday": "2019-12-05T19:37:04.129Z",
-//   "hiredDate": "2019-12-05T19:37:04.129Z",
-//   "isFullTime": true,
-//   "profileImage": "string",
-//   "jobTitle": "string",
-//   "jobDescription": "string",
-//   "phoneNumber": "string",
-//   "interestingFact": "string",
-//   "address": "string",
-//   "city": "string",
-//   "zip": "string",
-//   "state": "string",
-//   "salary": 0,
-//   "gender": "string",
-//   "email": "string",
-//   "emergencyContactPerson": "string",
-//   "emergencyContactPhone": "string",
-//   "emergencyContactAddress": "string",
-//   "ptoHours": 0,
-//   "companyKey": "string"
-// }
